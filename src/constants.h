fc6785d7bce4e65c804959f3f6cde38f124f23eb
Liam <liam.bowers@adp.com>
2017-11-17 12:59:50 -0500

    rename some constants; adjust weights

--- src/constants.h
+++ src/constants.h
@@ -16,7 +16,7 @@ const double DIRECTION_FORWARD = 1.0;
 const double DIRECTION_REAR = -1.0;
 
 // Total road distance of the the highway loop
-const double TRACK_DISTANCE = 6945.554;
+const double TRACK_LENGTH = 6945.554;
 
 // boundaries of acceptable speed of our vehicle
 const double SPEED_LIMIT = 20.75;  // speed in meters/s
@@ -31,10 +31,10 @@ const double FRONT_BUFFER = FORWARD_GAP_THRESHOLD + 5.0;
 const double SPEED_BUFFER = 1.0;
 
 // Weights for the behavior planner
-const double PREFER_MID_LANE_WEIGHT = 0.5;
+const double PREFER_MID_LANE_WEIGHT = 0.25;
+const double LANE_CHANGE_WEIGHT = 1.75;
 const double REAR_GAP_WEIGHT = 0.5;
 const double FORWARD_GAP_WEIGHT = 1.0;
-const double LANE_CHANGE_WEIGHT = 1.6;
 
 typedef enum { LEFT, MID, RIGHT, OUT_OF_BOUNDS } Lane;
 typedef enum { KEEP_LANE, TURN_RIGHT, TURN_LEFT } Behavior;
